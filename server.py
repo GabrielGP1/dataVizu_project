c227525af86c782bc9b76c67b5134323be833cac
GabrielGP1 <g.gascon-parent@hotmail.com>
2025-06-16 17:43:06 -0400

    change run

--- server.py
+++ server.py
@@ -19,4 +19,5 @@ def create_app():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    create_app().run(host="0.0.0.0", port=port, debug=True)
+    create_app().run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+
