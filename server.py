06ee54205e0b1ecacdf331a71434b4b9608d6c44
GabrielGP1 <g.gascon-parent@hotmail.com>
2025-06-16 17:46:00 -0400

    small fix

--- server.py
+++ server.py
@@ -18,6 +18,6 @@ def create_app():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
+    port = int(os.environ.get("PORT", 8085))
     create_app().run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
